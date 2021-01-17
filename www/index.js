28893153860130e0a4d7d890b8e9a5b70a7bf45e
Turtlemay <am@trtl.in>
2021-01-16 23:44:42 -0500

    Formatting.

--- www/index.js
+++ www/index.js
@@ -1,25 +1,25 @@
 addEventListener("DOMContentLoaded", async function _render() {
-  const appVersion = await fetchVersionFromJSON(
-    "https://raw.githubusercontent.com/turtlemay-target/target-cashier/main/package.json"
-  );
-  const extVersion = await fetchVersionFromJSON(
-    "https://raw.githubusercontent.com/turtlemay-target/turtlemay-target/main/manifest.json"
-  );
+	const appVersion = await fetchVersionFromJSON(
+		"https://raw.githubusercontent.com/turtlemay-target/target-cashier/main/package.json"
+	);
+	const extVersion = await fetchVersionFromJSON(
+		"https://raw.githubusercontent.com/turtlemay-target/turtlemay-target/main/manifest.json"
+	);
 
-  const appTextEl = document.querySelector("[data-id='app-version']");
-  const extTextEl = document.querySelector("[data-id='ext-version']");
+	const appTextEl = document.querySelector("[data-id='app-version']");
+	const extTextEl = document.querySelector("[data-id='ext-version']");
 
-  if (appVersion && appTextEl) appTextEl.value = appVersion;
-  if (extVersion && extTextEl) extTextEl.value = extVersion;
+	if (appVersion && appTextEl) appTextEl.value = appVersion;
+	if (extVersion && extTextEl) extTextEl.value = extVersion;
 });
 
 async function fetchVersionFromJSON(url) {
-  let res, json;
-  try {
-    res = await fetch(url);
-    json = await res.json();
-  } catch (e) {
-    console.error(e);
-  }
-  return json["version"] ?? null;
+	let res, json;
+	try {
+		res = await fetch(url);
+		json = await res.json();
+	} catch (e) {
+		console.error(e);
+	}
+	return json["version"] ?? null;
 }
